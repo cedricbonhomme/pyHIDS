ec65d1919f23ac2ec2a8242b37d79ef231cdae48
cedricbonhomme <devnull@localhost>
2010-03-07 14:31:27 +0100

    Bug fix: TypeError, Unicode-objects must be encoded before hashing.

--- pyHIDS.py
+++ pyHIDS.py
@@ -112,7 +112,7 @@ def compare_hash(target_file, expected_hash):
 
     # now we're ready to compare the hash values
     if opened_file is not None:
-        sha256_hash.update(data)
+        sha256_hash.update(data.encode())
         hashed_data = sha256_hash.hexdigest()
 
         if hashed_data == expected_hash:
