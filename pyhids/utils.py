346d62838850d659a9268c6a7c644aca04b47b56
Cédric Bonhomme <cedric@cedricbonhomme.org>
2023-10-06 23:41:18 +0200

    Removed useless commented part of code.

--- pyhids/utils.py
+++ pyhids/utils.py
@@ -11,9 +11,4 @@ def load_base():
     database = None
     with open(conf.DATABASE, "rb") as serialized_database:
         database = pickle.load(serialized_database)
-    # try:
-    #     base_file = open(conf.DATABASE, "r")
-    # except:
-    #     globals()['warning'] = globals()['warning'] + 1
-    #     log("Base file " + conf.DATABASE + " does no exist.")
     return database
